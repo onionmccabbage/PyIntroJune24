5d3e73e755f58585d51db95d12bc93f2f8e383b5
onionmccabbage <onionmccabbage@live.com>
2024-06-12 10:25:07 +0100

    initial commit

---
+++ basics.py
@@ -0,0 +1,12 @@
+# Python comments look like this
+# All python code is written in a module like this
+# Variables, data types and collections
+
+a = 3   # this is an integer data type
+b = 7.4 # this is a floating point data type
+
+print(a, type(a) ) # NB Python is case-sensitive so keep an eye out...
+print(b, type(b) ) 
+
+print( a/b , a*b, a+b, b-a )
+
