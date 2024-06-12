c93984b2aaa6feb42bf592fd33aea04133f00c83
onionmccabbage <onionmccabbage@live.com>
2024-06-12 10:41:59 +0100

    using  slicing

--- basics.py
+++ basics.py
@@ -9,4 +9,12 @@ print(a, type(a) ) # NB Python is case-sensitive so keep an eye out...
 print(b, type(b) ) 
 
 print( a/b , a*b, a+b, b-a )
+# NB Epsilon is the small margin of error ALL computers will encounter
 
+s = 'changed'
+s = 'is it coffee yet?' # a string data-type
+print(s, type(s), len(s) ) # string is an immutable collection of characters
+
+# we can use slicing to access members of a string collection
+print(s[4])# we expect t - all collections in Python are zero-based
+print(s[4:16:2]) # slicing is [start:stop-before:step]
