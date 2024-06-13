dfd9956cdc9f1d9508603da3cc675395ef55250a
onionmccabbage <onionmccabbage@live.com>
2024-06-13 12:03:37 +0100

    using map to repeatedly aply a function

--- functional_programming/using_fn_conventions.py
+++ functional_programming/using_fn_conventions.py
@@ -22,9 +22,21 @@ def sumDiff(x, y):
         result = util( (x,y) )
     return result
 
+# we can extend this idea of reuseability to map and filter...
+def useMap():
+    # we can repeatedly apply a function across a bunch of values using 'map'
+    my_values = range(-9, 10) # start, stop-before
+    results_l = map(sumDiff, my_values, my_values)
+    return results_l
+
 if __name__ == '__main__':
     # execise the code
     print( sumDiff(44, 55) ) # (99, -11)
     print( sumDiff('44', 55) ) # (99, -11)
-    print( sumDiff(True, 55) ) # 
+    print( sumDiff(True, 55) ) # Here the value True is being cast to a float...
     print( sumDiff('no', 'oops') ) # (1, -1)
+    # try using the map in 'useMap'
+    print( useMap() )
+    r = useMap()
+    for _ in r: # iterate over the map object - see eahc value that has been calculated
+        print( _ )
