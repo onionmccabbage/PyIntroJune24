399c3eff69bacda3ad9a656e26cd922af468e79c
onionmccabbage <onionmccabbage@live.com>
2024-06-13 11:00:17 +0100

    simple class inheritance

---
+++ my_classes/class_b.py
@@ -0,0 +1,6 @@
+# any class may inherist from any other class
+from class_a import Volunteer
+
+class VolunteerGroup(Volunteer):
+    '''Everything in the volunteer class is now available in this class'''
+
